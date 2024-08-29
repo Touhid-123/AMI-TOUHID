dd2be4a61b32acc1ccde1953806c606440884c4b
Touhid-123 <mff860981@gmail.com>
2024-08-29 09:12:57 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -62,7 +62,7 @@ module.exports = {
       const totalCommands = commands.size;
       msg += `\n\n╭─────❃[ 𝗘𝗻𝗷𝗼𝘆 ]\n│> 𝗧𝗼𝘁𝗮𝗹 𝗰𝗺𝗱𝘀: [${totalCommands}].\n│𝗧𝘆𝗽𝗲: [ ${prefix}𝗵𝗲𝗹𝗽 𝘁𝗼 \n│<𝗰𝗺𝗱> 𝘁𝗼 𝗹𝗲𝗮𝗿𝗻 𝘁𝗵𝗲 𝘂𝘀𝗮𝗴𝗲.]\n╰────────────✦`;
       msg += ``;
-      msg += `\n╭─────❃\n│🌟 | [ 𝗠𝗥.𝗔𝗬𝗔𝗡 ]\n│https://m.me/NOOBS.DEVELOPER.AYAN\n╰────────────✦`; // its not decoy so change it if you want 
+      msg += `\n╭─────❃\n│🌟 | [ MR.TOUHID ]\n│https://m.me/NOOBS.DEVELOPER.AYAN\n╰────────────✦`; // its not decoy so change it if you want 
 
 
       await message.reply({
